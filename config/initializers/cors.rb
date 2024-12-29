50b89761857ff01d3efabde7ed88ea6f5eb0973c
hamaguri-k <karuma00711@gmail.com>
2024-12-29 22:25:12 +0900

    change URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001'
+      origins 'https://bento-app2.onrender.com'
   
       resource '*',
         headers: :any,
--- frontend/src/url/index.jsx
+++ frontend/src/url/index.jsx
@@ -1,4 +1,4 @@
-const COMMON_URL = "https://bento-app.onrender.com/"
+const COMMON_URL = "https://bento-app.onrender.com"
 
 export const SHIFT_URL = `${COMMON_URL}/shifts`;
 export const RESULT_URL = `${COMMON_URL}/results`;
