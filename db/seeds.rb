5f7468c880d9f2a18b48a5ae02f1cf15a21fc835
hamaguri-k <karuma00711@gmail.com>
2024-12-29 21:05:21 +0900

    add seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Shift.create([
+  { name: "oishi",
+  monday: false,
+  tuesday: false,
+  wednesday: false,
+  thursday: false,
+  friday: false },
+  { name: "Nanami",
+  monday: false,
+  tuesday: false,
+  wednesday: false,
+  thursday: false,
+  friday: false },
+  { name: "Ishikawa",
+  monday: false,
+  tuesday: false,
+  wednesday: false,
+  thursday: false,
+  friday: false },
+  { name: "Nakajima",
+  monday: false,
+  tuesday: false,
+  wednesday: false,
+  thursday: false,
+  friday: false },
+  { name: "Suzuki",
+  monday: false,
+  tuesday: false,
+  wednesday: false,
+  thursday: false,
+  friday: false }
+])
+Result.create([
+    { day: "monday" },
+    { day: "tuesday" },
+    { day: "wednesday" },
+    { day: "thursday" },
+    { day: "friday" }
+  ])
