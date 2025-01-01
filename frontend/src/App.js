2aba317eca4212ae8c8356d400bab07150a6008b
hamaguri-k <karuma00711@gmail.com>
2025-01-01 18:32:28 +0900

    hashrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Input } from './containers/Input';
 import { Result } from './containers/Result';
 import { Admin } from './containers/Admin';
